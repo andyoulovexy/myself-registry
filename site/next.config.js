b897f84bb0189bc06f546fc2f56d4d95b12bab69
andyoulovexy <38940994+andyoulovexy@users.noreply.github.com>
2023-05-25 16:36:26 +0800

    Update next.config.js
    just create myself registry

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Ai-Desk Technologies',
+    description: 'The official store for Ai-Desk workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://andyoulovexy.github.io/myself-registry/',
+    contactUrl: 'https://github.com/andyoulovexy/myself-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/myself-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
